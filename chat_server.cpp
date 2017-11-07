299cf65415306aa73350c617ab2e21ec6ad8dc28
NhanTVu <uvnahn21@gmail.com>
2017-11-06 19:40:51 -0600

    fixed repeated file4

--- chat_server.cpp
+++ chat_server.cpp
@@ -1,278 +1,261 @@
+
+//
+// chat_server.cpp
+// ~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2013 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <cstdlib>
+#include <algorithm>
 #include <vector>
-#include <sstream>
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <deque>
 #include <iostream>
+#include <list>
+#include <memory>
+#include <set>
+#include <utility>
+#include <boost/asio.hpp>
 #include <string.h>
-#include <cstdlib>
-#include <boost/algorithm/string/split.hpp>
-#include <boost/algorithm/string/classification.hpp>
+#include "chat_server.h"
+#include "chat_message.hpp"
+
+using boost::asio::ip::tcp;
 
 
 
 
-class users
-{
-private:
-  int uuid;
-  std::string nickname;
-  std::vector<std::string> muted_list;
-public:
-  users(int u_uuid, std::string u_nick) : uuid(u_uuid), nickname(u_nick) {}
-  int get_uuid()
-  {
-    return uuid;
-  }
-  std::string get_nick()
-  {
-    return nickname;
-  }
-  void set_nick(std::string name)
-  {
-    nickname = name;
-  }
-  void set_uuid(int num)
-  {
-    uuid = num;
-  }
-  void mute_user(std::string name)
-  {
-    muted_list.push_back(name);
-  }
-};
+//----------------------------------------------------------------------
 
-std::vector<std::string> chatrooms_list;
-std::vector<int> uuid_vector;
-std::vector<users> user_list;
+typedef std::deque<chat_message> chat_message_queue;
 
-class chatrooms                             // Will get moved to class chat_room
+//----------------------------------------------------------------------
+
+class chat_participant
 {
 public:
-  chatrooms(bool in_private_id, std::string in_name) : private_id(in_private_id), name(in_name) {
-    chatrooms_list.push_back(in_name);
-  }
-  void message_backlog(std::string message) //read from the vector from the last element to the first element
-  {
-    if (messages.size() < 101)
-    {
-      messages.push_back(message);
-    }
-    else
-    {
-      messages.erase(messages.begin()+0);
-    }
-  }
-    std::string get_name()
-    {
-      return name;
-    }
-    bool is_private()
-    {
-      return private_id;
-    }
-  
-
-
-private:
-  bool private_id;
-  std::string name;
-  std::vector<users> chatroom_users;
-  std::vector<std::string> messages;
+  virtual ~chat_participant() {}
+  virtual void deliver(const chat_message& msg) = 0;
 };
 
+typedef std::shared_ptr<chat_participant> chat_participant_ptr;
 
-std::string nickname_handle(int uuid)
+//----------------------------------------------------------------------  
+
+//----------------------------------------------------------------------------------------------------------
+
+
+
+//-------------------------------------------------------------------------------------------------------------
+
+class chat_room
+{
+public:
+  void join(chat_participant_ptr participant)
   {
-    for (int i = 0; i < user_list.size(); i++)
+    participants_.insert(participant);
+    for (auto msg: recent_msgs_)
+      participant->deliver(msg);
+  }
+
+  void leave(chat_participant_ptr participant)
+  {
+    participants_.erase(participant);
+  }
+
+  void deliver(const chat_message& msg)
+  {
+    recent_msgs_.push_back(msg);
+    while (recent_msgs_.size() > max_recent_msgs)
+      recent_msgs_.pop_front();
+
+    for (auto participant: participants_)
+      participant->deliver(msg);
+  }
+
+private:
+  std::set<chat_participant_ptr> participants_;
+  enum { max_recent_msgs = 100 };
+  chat_message_queue recent_msgs_;
+};
+
+//----------------------------------------------------------------------
+
+class chat_session
+  : public chat_participant,
+    public std::enable_shared_from_this<chat_session>
+{
+public:
+  chat_session(tcp::socket socket, chat_room& room)
+    : socket_(std::move(socket)),
+      room_(room)
+  {
+  }
+
+  void start()
+  {
+    room_.join(shared_from_this());
+    do_read_header();
+  }
+
+  void deliver(const chat_message& msg)
+  {
+    bool write_in_progress = !write_msgs_.empty();
+    write_msgs_.push_back(msg);
+    if (!write_in_progress)
     {
-      if (user_list[i].get_uuid() == uuid)
-      {
-        return user_list[i].get_nick();
-      }
+      do_write();
     }
   }
-void change_nick(int uuid, std::string name)
-  {
-    for (int i = 0; i < user_list.size(); i++)
-    {
-      if (user_list[i].get_uuid() == uuid)
-      {
-        user_list[i].set_nick(name);
-      }
-    }
-  }
-  int requuid_handle()
-  {
-    bool val = false;
-    int num = 0000;
-    while (!val)
-    {
-      srand(time(NULL));
-      num = rand()%1000000000;
-      if (std::find(uuid_vector.begin(), uuid_vector.end(), num) != uuid_vector.end())
-      {
-        
-      }
-      else
-      {
-        val = true;
-      }
-    }
-    return num; 
-  }
-  std::vector<std::string> chatroom_handle() //Will provide the vector containing the names of the chat rooms
-  {
-    return chatrooms_list;
-  }
-  void create_chatroom(std::string name)
-  {
 
+private:
+  void do_read_header()
+  {
+    auto self(shared_from_this());
+    boost::asio::async_read(socket_,
+        boost::asio::buffer(read_msg_.data(), chat_message::header_length),
+        [this, self](boost::system::error_code ec, std::size_t /*length*/)
+        {
+          if (!ec && read_msg_.decode_header())
+          {
+            do_read_body();
+          }
+          else
+          {
+            room_.leave(shared_from_this());
+          }
+        });
   }
 
-  std::vector<std::string> request_users()
+  void do_read_body()
   {
-    int i = 0;
-    std::string return_value;
-    std::vector<std::string> tmp;
-    while (i < user_list.size())
-    {
-      int uuid = user_list[i].get_uuid();
-      std::string nick = user_list[i].get_nick();
-      return_value = std::to_string(uuid) + ',' + nick;
-      tmp.push_back(return_value);
-    }
-    return tmp;
-  }
-
-  int user_uuid(int i)                                                        //user_uuid()
-  {
-    return user_list[i].get_uuid();
-  }
-
-  std::string user_nick(int i)                                                //user_nick()
-  {
-    return user_list[i].get_nick();
-}
-
-
-std::string parseChecksum(std::string input){
-  //get everything up to first space
-  std::string checksum = input.substr(0,input.find(' '));
-  return checksum;
-}
-
-int parseTime(std::string input){
-  //find first space and start a substring at the next index
-  std::string temp = input.substr(input.find(' ')).erase(0,1); 
-  //take everything up until 2nd space and convert to int in base 10
-  int time = std::stoi(temp.substr(0,temp.find(' ')),nullptr,10);
-  return time;   
-}
-
-std::string parseCmd(std::string input){
-/*  std::string cmd;
-  std::vector<std::string> results;
-  std::stringstream s(input);
-  while(!s.eof())
-  {
-    std::string tmp;
-    s >> tmp;
-    results.push_back(tmp);
-  }
-*/
-  std::string cmd = input.substr(input.find(' ')).erase(0,1); 
-  //find 2nd space and start a substring at the next index
-  cmd = cmd.substr(cmd.find(' ')).erase(0,1);
-  std::cout<<cmd<<'\n';
-  return cmd;
-
-  //std::cout<<results[2]<<'\n';
-  //return results[2];
-}
-
-
-//take command and execute the command
-//if the command have arguements (such as NAMECHATROOM) it will also parse the optional command
-void ExecCmd(std::string cmd){
-  //request uuid
-  if(cmd == "REQUUID"){
-    //debugger print  
-    //int uuid = requuid_handle();
-    //int uuid = 1234;
-    //std::string value = std::to_string(uuid);
-    //return value;
-    std::cout<<"REQUUID ran successfully"<<'\n';
-  //set nickname
-  }else if(cmd.substr(0,5).compare("NICK ")==0){
-    
-    //truncate "NICK " from string
-    std::string cmdOption = cmd.substr(5,cmd.length()-5);
-
-    //debugger print
-    std::cout<<"NICK ran successfully"<<'\n';
-            
-    //debugger print for input after space
-    //std::cout<<"Actual input: "<<input<<'\n';
-
-    //check if new nickname is > 10
-    //if(input.length()>10){
-      //std::cout<<"Error! Nickname cannot exceed 10 characters"<<'\n';
-    //}
-    //else{
-      //debugger print
-      //std::cout<<input.length()<<'\n';
-     // std::cout<<"NICK ran successfully"<<'\n';
-    //}
-      //request list of available chatroom
-  }else if(cmd.compare("REQCHATROOMS")==0){
-    //debugger print
-    std::cout<<"REQCHATROOMS ran successfully"<<'\n';
-
-  //rename chatroom
-  }else if(cmd.substr(0,13).compare("NAMECHATROOM ")==0){
-    //truncate "NAMECHATROOM " from string
-    std::string cmdOption = cmd.substr(13,cmd.length()-13);
-
-    //debugger print
-    std::cout<<"NAMECHATROOM ran successfully"<<'\n';
-
-    //debugger print for input after space
-    //std::cout<<"Actual input: "<<input<<'\n';
-
-  //send a text
-  }else if(cmd.substr(0,9).compare("SENDTEXT ")==0){
-    //debugger print
-    std::cout<<"SENDTEXT ran successfully"<<'\n';
+    auto self(shared_from_this());
+    boost::asio::async_read(socket_,
+        boost::asio::buffer(read_msg_.body(), read_msg_.body_length()),
+        [this, self](boost::system::error_code ec, std::size_t /*length*/)
+        {
           
-    //truncate "SENDTEXT " from string
-    std::string cmdOption = cmd.substr(9,cmd.length()-9);
-        
-    //debugger print for input after space
-    std::cout<<"Actual input: "<<cmdOption<<'\n';
-
-    //check for ; in text
-    if(cmd.find(";") != std::string::npos){
-      std::cout<<"Error! Message cannot contain ';' in it"<<'\n';   
-      }
-    //finally if conditions are all met send message
-    else{ 
-      //debugger print
-      std::cout<<"SENDTEXT ran successfully"<<'\n';
-    }
-
-  //request text
-  }else if(cmd.compare("REQTEXT")==0){
-    //debugger print
-    std::cout<<"REQTEXT ran successfully"<<'\n';
-
-    //request users in chatroom
-  }else if(cmd.compare("REQUSERS")==0){
-    //debugger print
-    std::cout<<"REQUSERS ran successfully"<<'\n';
-  }else{
-    std::cout<<"Error! Your entry does not fit the standard format."<<'\n';
-    std::cout<<"Type 'Help' for a list of format and their functions"<<'\n';
+          if (!ec)
+          {
+            std::stringstream buffer;
+            //std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+            buffer.write(read_msg_.body(), read_msg_.body_length());
+            //std::cout << "\n";
+            //std::string text = buffer.str();
+            std::cout << "|"<<buffer.str() <<"|"<<std::endl; 
+            //std::string poi = "REQUUID"; 
+            //int num1 = text.length();                      //buffer is the chat_message converted into a string
+            std::string value = parseCmd(buffer.str());
+            //value.erase(remove_if(value.begin(), value.end(), isspace), value.end());
+            //int num2 = value.length();
+            //std::cout << value << "s" << std::endl; 
+            ExecCmd(value);
+            //std::cout <<num1 << num2 << std::endl;                        //buffer is the chat_message converted into a string
+            room_.deliver(read_msg_);
+            do_read_header();
+          }
+          else
+          {
+            room_.leave(shared_from_this());
+          }
+        });
   }
 
+  void do_write()
+  {
+    auto self(shared_from_this());
+    boost::asio::async_write(socket_,
+        boost::asio::buffer(write_msgs_.front().data(),
+          write_msgs_.front().length()),
+        [this, self](boost::system::error_code ec, std::size_t /*length*/)
+        {
+          if (!ec)
+          {
+            write_msgs_.pop_front();
+            if (!write_msgs_.empty())
+            {
+              do_write();
+            }
+          }
+          else
+          {
+            room_.leave(shared_from_this());
+          }
+        });
+  }
+
+  tcp::socket socket_;
+  chat_room& room_;
+  chat_message read_msg_;
+  chat_message_queue write_msgs_;
+};
+
+//----------------------------------------------------------------------
+
+class chat_server
+{
+public:
+  chat_server(boost::asio::io_service& io_service,
+      const tcp::endpoint& endpoint)
+    : acceptor_(io_service, endpoint),
+      socket_(io_service)
+  {
+    do_accept();
+  }
+
+private:
+  void do_accept()
+  {
+    acceptor_.async_accept(socket_,
+        [this](boost::system::error_code ec)
+        {
+          if (!ec)
+          {
+            std::make_shared<chat_session>(std::move(socket_), room_)->start();
+          }
+
+          do_accept();
+        });
+  }
+
+  tcp::acceptor acceptor_;
+  tcp::socket socket_;
+  chat_room room_;
+};
+
+//----------------------------------------------------------------------
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc < 2)
+    {
+      std::cerr << "Usage: chat_server <port> [<port> ...]\n";
+      return 1;
+    }
+
+    boost::asio::io_service io_service;
+
+    std::list<chat_server> servers;
+    for (int i = 1; i < argc; ++i)
+    {
+      tcp::endpoint endpoint(tcp::v4(), std::atoi(argv[i]));
+      servers.emplace_back(io_service, endpoint);
+    }
+
+    io_service.run();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
 }

231d52b5ee168322ec657811be36e344f4c3c2d4
AndrewRyan135 <andrewbreak62@gmail.com>
2017-11-13 01:18:46 -0600

    Beginning to implement chatrooms

--- chat_server.cpp
+++ chat_server.cpp
@@ -84,6 +84,28 @@ private:
   chat_message_queue recent_msgs_;
 };
 
+/*void chat_room::join(chat_participant_ptr participant)
+{
+    participants_.insert(participant);
+    for (auto msg: recent_msgs_)
+    participant->deliver(msg);
+}
+
+void chat_room::leave(chat_participant_ptr participant)
+  {
+    participants_.erase(participant);
+  }
+
+void chat_room::deliver(const chat_message& msg)
+  {
+    recent_msgs_.push_back(msg);
+    while (recent_msgs_.size() > max_recent_msgs)
+      recent_msgs_.pop_front();
+
+    for (auto participant: participants_)
+      participant->deliver(msg);
+  }*/
+
 //----------------------------------------------------------------------
 
 class chat_session
